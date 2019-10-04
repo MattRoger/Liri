7ecf68ef0e76b1ae6aba56b31f4a391220903dcc
MattRoger <mrogerphotography@gmail.com>
2019-10-04 09:16:59 -0600

    movies and concert working

--- keys.js
+++ keys.js
@@ -1,11 +1,22 @@
 require("./liri.js");
+require("dotenv").config({ path: ".env" });
 
 console.log('this is loaded');
 
+function Spotify(id, secret){
+  this.id=id,
+  this.secret=secret
+}
+
+var spotify= new Spotify(
+  process.env.SPOTIFY_ID, process.env.SPOTIFY_SECRET
+)
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+
 
 
 
--- liri.js
+++ liri.js
@@ -1,11 +1,15 @@
-require("dotenv").config({path:".env"});
+require("dotenv").config({ path: ".env" });
 require("./keys.js");
+require('node-spotify-api');
+console.log("here we are")
 require("fs");
+// var spotify=require('node-spotify-api');
 
-var Spotify=require('node-spotify-api');
+// var spotify = new Spotify(keys.spotify);
+
+
 var inquirer = require("inquirer");
 var axios = require("axios");
-var spotify = new Spotify(keys.spotify);
 
 var typeSearch = process.argv[2]
 var searchContent = (process.argv.slice(3))
@@ -16,14 +20,13 @@ var searchContent = (process.argv.slice(3))
 if (typeSearch === "concert-this") {
     bands();
 };
-if (typeSearch === "movie-this"){
+if (typeSearch === "movie-this") {
     movie();
 }
-if(typeSearch="spotify-this-song"){
-    console.log("thus worked")
-    spotifyx();
-
-}
+// if (typeSearch = "spotify-this-song") {
+//     console.log("thus worked")
+//     spotifyx();
+// }
 
 function bands() {
     var artist = searchContent.join("");
@@ -40,42 +43,41 @@ function bands() {
             })
 
 }
-function movie(){
-    var movie =searchContent.join("+");
+function movie() {
+    var movie = searchContent.join("+");
     console.log(movie)
-    axios.get("http://www.omdbapi.com/?t="+movie+
-    "&y=&plot=short&apikey=trilogy").then(
-    function(response){
-        var res=response.data;
-        var title=res.Title;
-        var year=res.Year;
-        var iRating=res.imdbRating;
-        var rTrating=res.Ratings[1].Value;
-        var country=res.Country;
-        var lang=res.Language;
-        var plot=res.Plot;
-        var actors=res.Actors;
-        console.log(
-        `${title}, ${year} + staring ${actors}
+    axios.get("http://www.omdbapi.com/?t=" + movie +
+        "&y=&plot=short&apikey=trilogy").then(
+            function (response) {
+                var res = response.data;
+                var title = res.Title;
+                var year = res.Year;
+                var iRating = res.imdbRating;
+                var rTrating = res.Ratings[1].Value;
+                var country = res.Country;
+                var lang = res.Language;
+                var plot = res.Plot;
+                var actors = res.Actors;
+                console.log(
+                    `${title}, ${year} + staring ${actors}
         ${plot}
         Imdb rates this movie ${iRating} and Rotten Tomatoes rates it ${rTrating} fresh.
         ${title} was filmed in ${country} and produced in ${lang}.`)
-    }
-    )
+            }
+        )
 }
-function spotifyx(){
-    console.log("spotify function connected")  
-    var spotifySearch=searchContent.join(" ")
-    console.log(spotifySearch);
-    spotify.search({ type: 'track', query: spotifySearch }, function(err, data) {
-        console.log(response.data)
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-       
-      console.log(data); 
-      });
-}
+// function spotifyx() {
+
+//     console.log("spotify function connected")
+//     var spotifySearch = searchContent.join(" ")
+//     console.log(spotifySearch);
+//     spotify
+//     .search({ type: 'track', query: spotifySearch })
+//     .then(function (response){
+//     console.log("response.data")        
+
+//     })
+// }
 
 
 
